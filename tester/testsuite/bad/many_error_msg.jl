a950354a44ecb2fc4bb329f6fe10bf76c13c66ca
hjorthjort <rikard.hjort@gmail.com>
2019-04-05 22:37:18 +0200

    Just a program which can emit a lot of error messages

---
+++ tester/testsuite/bad/many_error_msg.jl
@@ -0,0 +1,23 @@
+int a() {
+    return b(true, 1);
+}
+
+void b(int i, boolean x) {
+   return 1;
+}
+
+void c() {
+    int x = 4, y = true;
+}
+
+void d() {
+    int d;
+}
+
+int d() {
+    if (4 > 2) {
+       return true;
+    } else {
+       x++;
+    }
+}
