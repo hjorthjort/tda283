5a367230a716c4c99d2cfb09a7700b472450b38a
Ayberk Tosun <ayberk.tosun@gmail.com>
2019-04-13 13:44:26 +0200

    Add some simple arithmetic tests for the compiler

---
+++ tester/testsuite/good/simple_intarith2.jl
@@ -0,0 +1,14 @@
+int main() {
+  int x = 3;
+  int y = 7;
+  int z = x + y;
+  z++;
+  z++;
+  int a = 15;
+  int b = a + 18;
+  b--;
+  b--;
+  b--;
+  printInt(z + b);
+  return 0;
+}
---
+++ tester/testsuite/good/simple_intarith3.jl
@@ -0,0 +1,7 @@
+int main() {
+  int x = 3;
+  int y = 2;
+  int z = 10;
+  printInt(x * y + z - 9);
+  return 0;
+}
