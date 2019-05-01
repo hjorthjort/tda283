bd6d3dbe17218f3731728cd8f5eb08ba512234e7
hjorthjort <rikard.hjort@gmail.com>
2019-05-01 15:00:44 +0200

    Tests on for-loops and length

---
+++ tester/testsuite/extensions/arrays1/index_variable_capture.jl
@@ -0,0 +1,7 @@
+int main() {
+    int for_index = 99;
+    int [] a = new int [1];
+    for (int x : a)
+        printInt(for_index);
+    return 0;
+}
---
+++ tester/testsuite/extensions/arrays1/length_is_int.jl
@@ -0,0 +1,5 @@
+int main() {
+    double [] a = new double[10];
+    boolean x = 10 < a.length;
+    return a.length;
+  }
