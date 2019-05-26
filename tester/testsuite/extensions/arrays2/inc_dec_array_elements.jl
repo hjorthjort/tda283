46aa0115c0b2e49339100873188c7db8f4be9c9d
Ayberk Tosun <ayberk.tosun@gmail.com>
2019-05-26 10:36:14 +0200

    Update the inc-dec test

--- tester/testsuite/extensions/arrays2/inc_dec_array_elements.jl
+++ tester/testsuite/extensions/arrays2/inc_dec_array_elements.jl
@@ -1,7 +1,12 @@
 int main() {
   int [] a = new int[2];
   a[0]++;
+  printInt(a[0]); // must print 1.
   int [][] b = new int[2][1];
   b[0][0]--;
+  b[1][1]++;
+  b[1][1]++;
+  printInt(b[0][0]); // must print -1.
+  printInt(b[1][1]); // must print 2.
   return 0;
 }
