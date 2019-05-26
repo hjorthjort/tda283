e4e4a3fb8958aba9d3c4ebe62ce3bdbecbbd0afa
Ayberk Tosun <ayberk.tosun@gmail.com>
2019-05-26 16:20:58 +0200

    Improve the `lengths` test

--- tester/testsuite/extensions/arrays2/lengths.jl
+++ tester/testsuite/extensions/arrays2/lengths.jl
@@ -3,5 +3,7 @@ int main() {
   printInt(xs.length);         // must print 3.
   printInt(xs[0].length);      // must print 4.
   printInt(xs[0][0].length);   // must print 5.
+  printInt(xs[0][1].length);   // must print 5.
+  printInt(xs[1][1].length);   // must print 5.
   return 0 ;
 }
