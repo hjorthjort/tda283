455c6736061503a89f225d18d50d2e9b3bf060a2
Ayberk Tosun <ayberk.tosun@gmail.com>
2019-05-26 16:16:36 +0200

    Add minimal example of the arrays2 problem

---
+++ tester/testsuite/extensions/arrays2/lengths.jl
@@ -0,0 +1,7 @@
+int main() {
+  int[][][] xs = new int[3][4][5];
+  printInt(xs.length);         // must print 3.
+  printInt(xs[0].length);      // must print 4.
+  printInt(xs[0][0].length);   // must print 5.
+  return 0 ;
+}
